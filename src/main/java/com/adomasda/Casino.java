6ea58e719dce2c7625867f9d7781eff764e58e15
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-06 11:14:26 +0300

    added GameType, GamingArena

--- src/main/java/com/adomasda/Casino.java
+++ src/main/java/com/adomasda/Casino.java
@@ -1,5 +1,29 @@
 package com.adomasda;
 
+import java.util.ArrayList;
+import java.util.List;
+
+enum GameType {
+    POKER,
+    PACHINKO_MACHINE,
+    SLOT_MACHINE,
+    BLACKJACK
+}
+
+class GamingArea {
+    List<GameType> games;
+    GamingArea () {
+        this.games = new ArrayList<>();
+    }
+
+    private boolean addGame(GameType game) {
+        return this.games.add(game);
+    }
+    private boolean deleteGame(GameType game) {
+        return this.games.remove(game);
+    }
+}
+
 public class Casino {
     private boolean isRigged;
 
