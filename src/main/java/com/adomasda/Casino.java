997b450cc56a395c7aa5be3a09af612f83b86604
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-06 09:56:28 +0300

    init Casino

---
+++ src/main/java/com/adomasda/Casino.java
@@ -0,0 +1,13 @@
+package com.adomasda;
+
+public class Casino {
+    private boolean isRigged;
+
+    public boolean isItRigged() {
+        return isRigged;
+    }
+
+    public void setRigged(boolean isRigged) {
+        this.isRigged = isRigged;
+    }
+}
