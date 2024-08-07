d5854fb571e6ba279ac9d5b0b8cb6dd9a5b61082
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-07 12:44:22 +0300

    Removed GameArea, added Elevator, Floor, FloorConfig, [1..4]Floor, ServiceType

--- src/main/java/com/adomasda/Casino.java
+++ src/main/java/com/adomasda/Casino.java
@@ -1,7 +1,7 @@
 package com.adomasda;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 enum GameType {
     POKER,
@@ -10,28 +10,116 @@ enum GameType {
     BLACKJACK
 }
 
-class GamingArea {
-    List<GameType> games;
-    GamingArea () {
-        this.games = new ArrayList<>();
-    }
+enum ServiceType {
+    BAR,
+    SPA,
+    SHOP
+}
 
-    private boolean addGame(GameType game) {
-        return this.games.add(game);
-    }
-    private boolean deleteGame(GameType game) {
-        return this.games.remove(game);
+class FloorConfig {
+    public LinkedList<Object> items;
+
+    public FloorConfig(Object... elements) {
+        items = new LinkedList<>(Arrays.asList(elements));
     }
 }
 
-public class Casino {
-    private boolean isRigged;
+abstract class Floor {
+    FloorConfig floorConfig;
 
-    public boolean isItRigged() {
-        return isRigged;
+    private int index;
+
+    Floor(FloorConfig floorConfig, int index) {
+        this.floorConfig = floorConfig;
+        this.index = index;
     }
 
-    public void setRigged(boolean isRigged) {
-        this.isRigged = isRigged;
+    public boolean addItem(Object item) {
+        return floorConfig.items.add(item);
+    }
+
+    public int getIndex() {return this.index;}
+
+    abstract void enter();
+}
+
+class FirstFloor extends Floor {
+    FirstFloor(FloorConfig floorConfig) {
+        super(floorConfig, 1);
+    }
+
+    @Override
+    void enter() {
+        System.out.println("Entering First Floor with items: " + floorConfig.items);
+    }
+}
+
+class SecondFloor extends Floor {
+    SecondFloor(FloorConfig floorConfig) {
+        super(floorConfig, 2);
+    }
+
+    @Override
+    void enter() {
+        System.out.println("Entering Floor with items: " + floorConfig.items);
+    }
+}
+
+class ThirdFloor extends Floor {
+    ThirdFloor(FloorConfig floorConfig) {
+        super(floorConfig, 3);
+    }
+
+    @Override
+    void enter() {
+        System.out.println("Entering Third Floor with items: " + floorConfig.items);
+    }
+}
+
+class FourthFloor extends Floor {
+    FourthFloor(FloorConfig floorConfig) {
+        super(floorConfig, 4);
+    }
+
+    @Override
+    void enter() {
+        System.out.println("Entering Third Fourth with items: " + floorConfig.items);
+    }
+}
+
+
+interface Elevator {
+    void goToFloor(int floorNumber);
+}
+
+class Casino implements Elevator {
+    private LinkedList<Floor> floors;
+    private Floor currentFloor;
+
+    public Casino(Floor... floors) {
+        this.floors = new LinkedList<>(Arrays.asList(floors));
+        this.currentFloor = this.floors.get(0);
+    }
+
+    public boolean addFloor(Floor floor) {
+        return floors.add(floor);
+    }
+
+    public void goToFloor(int floorNumber) {
+        if (floorNumber <= 0) {
+            System.err.println("Enter a positive floor number");
+        }
+        floorNumber--;
+        int currentIndex = floors.indexOf(currentFloor);
+        while (currentIndex < floorNumber) {
+            System.out.println("Passing floor: " + floors.get(currentIndex).getIndex());
+            currentIndex++;
+        }
+        while (currentIndex > floorNumber) {
+            System.out.println("Passing floor: " + floors.get(currentIndex).getIndex());
+            currentIndex--;
+        }
+        currentFloor = floors.get(floorNumber);
+        currentFloor.enter();
     }
 }
--- src/main/java/com/adomasda/Main.java
+++ src/main/java/com/adomasda/Main.java
@@ -2,6 +2,16 @@ package com.adomasda;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println("TODO!");
+        FloorConfig firstFloorConfig = new FloorConfig(GameType.BLACKJACK, GameType.SLOT_MACHINE, ServiceType.SPA);
+        FloorConfig secondFloorConfig = new FloorConfig(GameType.PACHINKO_MACHINE, GameType.PACHINKO_MACHINE, ServiceType.SHOP);
+        Floor firstFloor = new FirstFloor(firstFloorConfig);
+        Floor secondFloor = new SecondFloor(secondFloorConfig);
+        Floor thirdFloor = new ThirdFloor(secondFloorConfig);
+
+        Casino casino = new Casino(firstFloor, secondFloor, thirdFloor);
+
+        casino.goToFloor(3);
+        casino.goToFloor(1);
+
     }
 }
